ed1bd2207a9f101534a25adceb9637de11b173fc
Clinton Ingalia <bicxtor1@gmail.com>
2023-12-21 12:09:08 +0300

    created alabsstore demo

---
+++ alabsstoredemo/alabsstoredemo/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-t2k6d##4i2v=g^_iw!6j94)isz0a*6sn^)deeiv5xqxe=)f$$d"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
