4a1b8ce2c8aa24216e5b5c8c8a2f6f77d3b43dab
Clinton Ingalia <bicxtor1@gmail.com>
2023-12-21 12:17:52 +0300

    created product model

--- alabsstoredemo/home/models.py
+++ alabsstoredemo/home/models.py
@@ -6,6 +6,25 @@ from wagtail.core.models import Page, Orderable
 from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 
+class Product(Page):
+    sku = models.CharField(max_length=255)
+    short_description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('sku'),
+        FieldPanel('price'),
+        ImageChooserPanel('image'),
+        FieldPanel('short_description'),
+        InlinePanel('custom_fields', label='Custom fields'),
+    ]
 
 class HomePage(Page):
     pass
